af22efa6944a1c336203a19556a28d7d8cf78e8f
CursedSensei <102786087+CursedSensei@users.noreply.github.com>
2023-08-01 03:22:52 +0800

    Revert "current song and is_ad functions"

--- LIBRARIES/spotify_api_test.py
+++
@@ -1,6 +0,0 @@
-# Description     : Code that will impress u ;)
-# Author          : G.M. Yongco
-# IMPORTS -----------------------------------------------------------------------------------------
-import spotify_api as sa
-
-print(f"yes\n\n{sa.current_playing_song()}")
---
+++ LIBRARIES/spotify_status.py
@@ -0,0 +1,16 @@
+# Description     : Code that will impress u ;)
+# Author          : G.M. Yongco && Yoshi Hirishima
+# IMPORTS -----------------------------------------------------------------------------------------
+import spotipy
+
+ID = open("./REFERENCES/client_ID.txt", "r").read()
+SECRET = open("./REFERENCES/client_secret.txt", "r").read()
+
+# MAIN --------------------------------------------------------------------------------------------
+
+def current_song():
+    print("hi there")
+
+    print(ID, SECRET)
+
+current_song()
