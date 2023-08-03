c3fe79b9bd09c453d7ae3b39a5fec948b6d7d718
Giulio Yongco <yongcogiulio125@gmail.com>
2023-08-03 15:24:50 +0800

    spotify_api.py changes
    dun need the imports, only needed them for the manual api stuff
    
    did guard clauses for the if statements
    
    made the client global

--- LIBRARIES/spotify_api.py
+++ LIBRARIES/spotify_api.py
@@ -1,9 +1,5 @@
 # Description     : Code that will impress u ;)
 # Author          : G.M. Yongco
-# IMPORTS -----------------------------------------------------------------------------------------
-import base64
-import requests #note to self: study this library
-
 # SPOTIPY IMPORTS ---------------------------------------------------------------------------------
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -12,22 +8,10 @@ ID = open("./REFERENCES/client_ID.txt", "r").read()
 SECRET = open("./REFERENCES/client_secret.txt", "r").read()
 REDIRECT_URI = open("./REFERENCES/redirect_uri.txt", "r").read()
 
+# FUNCTIONS ---------------------------------------------------------------------------------------
 
-# MAIN --------------------------------------------------------------------------------------------
-def result_is_none(result):
-	result = str(result)
-
-	return_val = 0
-	if(str(result) == 'None'):
-		return_val = 1
-	
-	return return_val
-
-# meant to be a boolean
-def current_playing_is_ad():
-	custom_scope = "user-read-currently-playing"
-
-	client = spotipy.Spotify(
+custom_scope = "user-read-currently-playing"
+current_playing_client = spotipy.Spotify(
 	auth_manager = SpotifyOAuth(
 			client_id       = ID, 
 			client_secret   = SECRET, 
@@ -36,37 +20,42 @@ def current_playing_is_ad():
 		)
 	)
 
-	result = client.currently_playing()
-	return_val = 0
+# meant to return a boolean
+def current_playing_is_ad():
+	result = current_playing_client.currently_playing()
+	return_val = False
 
-	if (result_is_none(result)):
+	if(str(result) == 'None'):
 		print("result is none")
 	elif(result['currently_playing_type'] == 'ad'):
-		return_val = 1
+		return_val = True
 		
 	return return_val
 
 def current_playing_song():
-	custom_scope = "user-read-currently-playing"
 
-	client = spotipy.Spotify(
-	auth_manager = SpotifyOAuth(
-			client_id       = ID, 
-			client_secret   = SECRET, 
-			scope           = custom_scope, 
-			redirect_uri    = REDIRECT_URI
-		)
-	)
-
-	result = client.currently_playing()
+	result = current_playing_client.currently_playing()
 	return_val = "currently_playing_type is an ad"
 
-	if(result_is_none(result)):
+	if(str(result) == 'None'):
 		return_val = "result_is_none"
 	elif(result['currently_playing_type'] == 'ad'):
 		return_val = "currently_playing_type is an ad"
 	else:
 		return_val = str(result['item']['name'])
-		print(return_val)
 
 	return return_val
+
+def get_device_id():
+	custom_scope = "user-read-playback-state"
+
+	client = spotipy.Spotify(
+	auth_manager = SpotifyOAuth(
+			client_id       = ID, 	
+			client_secret   = SECRET, 
+			scope           = custom_scope, 
+			redirect_uri    = REDIRECT_URI
+		)
+	)
+
+	return client.devices()
